1202bf2adb4f6e90c084a8ae443e3758d79097c5
Sim <foday_s@yahoo.com>
2023-02-18 22:03:16 +0000

    select where assignment completed

---
+++ problem1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee WHERE first_name = 'Steve' AND salary > 75000;
+--  SELECT * FROM table WHERE condition1 = value1 AND condition2 = value2;
+--  *      SELECT * FROM table WHERE something > 500 OR something < 100;
+--  *      SELECT * FROM table WHERE value NOT IN ('value1', 'value2');
---
+++ problem2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee WHERE salary < 50000 OR salary > 100000;
+--  SELECT * FROM table WHERE condition1 = value1 AND condition2 = value2;
+--  *      SELECT * FROM table WHERE something > 500 OR something < 100;
+--  *      SELECT * FROM table WHERE value NOT IN ('value1', 'value2');
